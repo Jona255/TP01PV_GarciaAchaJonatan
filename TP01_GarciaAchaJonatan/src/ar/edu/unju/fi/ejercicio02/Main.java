b84238118c6980ec3b9cf32b6d1809fa47f164f4
Rolo <sacha.255555@gmail.com>
2024-04-08 20:09:25 -0300

    Agregado el ejercicio 02

---
+++ TP01_GarciaAchaJonatan/src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -0,0 +1,33 @@
+package ar.edu.unju.fi.ejercicio02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String pais;
+		int edad;
+		double altura;
+		float producto;
+		long telefono;
+		double coseno;
+		
+		
+		pais= "Argentina";
+		edad=21;
+		altura = 2.10;
+		producto =  15000;
+		telefono = 154201292;
+		coseno =0.99996192306417128873735516482698;
+		
+		System.out.println("El pais es: "+pais);
+		System.out.println("La edad es: "+edad);
+		System.out.println("La altura es: "+ altura);
+		System.out.println("El precio del Producto es: "+producto);
+		System.out.println("El numero de telefono es: "+telefono);
+		System.out.println("el coseno de 0.5 es: "+coseno);
+		
+		
+		
+	}
+
+}
