ccb0de04a5af5994a7c5c55d345b41a92d12b917
Ralo <Ralo@DESKTOP-Q3VNS6C>
2024-04-13 20:18:31 -0300

    Completado el ejercicio0 07
    tuve un problema mas que nada visual a la hora de mostrar el salario con
    formato, de todas formas no afecta el funcionamiento del programa.

--- TP01_GarciaAchaJonatan/src/ar/edu/unju/fi/ejercicio07/main/Main.java
+++ TP01_GarciaAchaJonatan/src/ar/edu/unju/fi/ejercicio07/main/Main.java
@@ -1,10 +1,43 @@
 package ar.edu.unju.fi.ejercicio07.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio07.model.Empleado;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
  
+		
+		Scanner sc = new Scanner(System.in);
+		String nombre;
+		int legajo; 
+		double salario;
+		
+		Empleado empleado; 
+		
+		System.out.println("Ingrese nombre y apellido: ");
+		nombre = sc.nextLine();
+		
+		System.out.println("Ingrese legajo: ");
+		legajo = sc.nextInt();
+		
+		System.out.println("Ingrese el salario: ");
+		salario = sc.nextDouble();
+		
+		empleado = new Empleado(nombre, legajo, salario);
+		System.out.println("------------------------");
+		System.out.println("Datos del Empleado");
+		empleado.mostrarDatosEmpleado();
+		System.out.println("------------------------");
+		System.out.println("Aumentando salario");
+		empleado.aumentarSalario(true);
+		empleado.mostrarDatosEmpleado();
+		
+		
 	}
 
 }
--- TP01_GarciaAchaJonatan/src/ar/edu/unju/fi/ejercicio07/model/Empleado.java
+++ TP01_GarciaAchaJonatan/src/ar/edu/unju/fi/ejercicio07/model/Empleado.java
@@ -1,9 +1,38 @@
 package ar.edu.unju.fi.ejercicio07.model;
 
+import java.text.DecimalFormat;
+
 public class Empleado {
 	private String nombre;
 	private int legajo;
-	private float salario;
-	final double SALARIO_MINIMO = 210000.00;
-	 
+	private double salario;
+	final double SALARIO_MINIMO = 2100000.00;
+	final double AUMENTO_MERITO = 20000.00;
+
+	public Empleado(String nombre, int legajo, double salario) {
+		super();
+		this.nombre = nombre;
+		this.legajo = legajo;
+
+		if (this.salario < SALARIO_MINIMO) {
+			this.salario = SALARIO_MINIMO;
+		} else {
+			this.salario = salario;
+		}
+	}
+
+	public void mostrarDatosEmpleado() {
+ 		DecimalFormat formatoSalario = new DecimalFormat("#,##0.##");
+ 		
+		System.out.println("Nombre del Empleado: " + this.nombre);
+		System.out.println("Legajo: " + this.legajo);
+		System.out.println("Salario $: " + formatoSalario.format(this.salario));
+	}
+	
+	public void aumentarSalario(boolean merito) {
+		if(merito) {			
+			this.salario=this.salario+AUMENTO_MERITO;
+		}
+	}
+
 }
