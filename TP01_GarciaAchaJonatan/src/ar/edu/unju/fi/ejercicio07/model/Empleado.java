c94c73957a99f39676d0e88d5e217b5169711982
Rolo <sacha.255555@gmail.com>
2024-04-13 16:59:09 -0300

    agregados paquetes, y clases para empezar el ejercicio 07

---
+++ TP01_GarciaAchaJonatan/src/ar/edu/unju/fi/ejercicio07/model/Empleado.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.ejercicio07.model;
+
+public class Empleado {
+	private String nombre;
+	private int legajo;
+	private float salario;
+	final double SALARIO_MINIMO = 210000.00;
+	
+}
