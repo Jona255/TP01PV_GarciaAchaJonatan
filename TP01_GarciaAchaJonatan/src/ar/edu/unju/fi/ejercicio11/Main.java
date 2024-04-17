9afee0cded1c95749b29945ff0922477d25056cb
Rolo <sacha.255555@gmail.com>
2024-04-16 21:24:20 -0300

    Intentando resolver ejercicio 11 tp2

---
+++ TP01_GarciaAchaJonatan/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,41 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int j=40;
+//		int i;
+//		
+//		System.out.print(j+1);
+//		j = 80;
+//		
+//		for(int i = 37; i>=1; i= i-2) {
+//			System.out.print(j+" ");
+//			j=j+i;
+//			
+//		}
+//		for(int i = 1; i<=42; i = i+2) {
+//			System.out.print(j+" ");
+//			j=j-i;
+//		}
+//		System.out.println();
+		
+		
+		
+		
+		
+		
+		int j;
+		int i;
+		
+		i=2;
+		for(j=40; j>0; j--) {
+			System.out.print(j*2+" ");
+			i=i+j*2;
+			
+		}
+	}
+
+}
